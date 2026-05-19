0b955fde0d02734588babb472976628bb65f14a8
taalmeida04 <talitasilva040310@gmail.com>
2026-05-19 18:02:02 -0300

    Segundo commit

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
-import { StyleSheet, Text, View, TouchableOpacity, TextInput, Image } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, Image, inputSearch } from 'react-native';
 import Feather from '@expo/vector-icons/Feather';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
 
@@ -15,15 +16,26 @@ export default function App() {
       </View>
 
       {/*Inicio da Barra de Pesquisa*/}
-      <View style = {styles.containerSearch}>
+      <View style={styles.containerSearch}>
         <TextInput
-        placeholder='Digite o filme que deseja buscar'
+          placeholder='Digite o filme que deseja buscar'
+          style={styles.inputSearch}
         ></TextInput>
+        <TouchableOpacity>
+          <Feather name="search" size={24} color="black" />
+        </TouchableOpacity>
 
       </View>
 
       {/*Inicio do Banner*/}
 
+      <Text style = {styles.textBanner}> Em cartaz</Text>
+
+      <Image
+
+        source={require("./assets/img.jpg")}
+        style ={styles.imageBanner}
+      />
 
     </View>
   );
@@ -52,13 +64,38 @@ const styles = StyleSheet.create({
 
   },
   containerSearch: {
-    marginTop:20,
-    width:"90%",
-    backgroundColor:'white',
-    borderRadius:7,
-    padding:8,
-  }
+    marginTop: 20,
+    width: "90%",
+    backgroundColor: 'white',
+    borderRadius: 7,
+    padding: 8,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  inputSearch: {
+    height: 26,
+    padding: 8,
+    width: '100%'
+  },
+
+  //Banner
+
+  imageBanner: {
+    width:'90%',
+    height:200,
+    marginTop:15,
+    borderRadius:10
+  },
+
+ textBanner: {
+ color:'white',
+ width:'90%',
+ fontSize:27,
+ marginTop:15,
+ fontWeight:'bold',
+ 
+}
+
 });
 
 
-{/*inicio do banner*/}
